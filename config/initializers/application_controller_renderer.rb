3e8fe9495a5ee8718f6f14cfa95c24c2e6fef6ae
vagrant <vagrant@localhost.localdomain>
2020-07-08 10:12:35 +0000

    [Finish] 3-8

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6a30b733c0f8a8f55ef013f9187372ac0d8cf11ab803e5a329af5425a23c99d26c317e75ca0d351ef883c2385ee47fc7b4141b238e3ff3863be6695cd87f9112'
